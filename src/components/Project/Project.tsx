23823fdee86a970893fb834f9be2aa2aecd9b73d
samir aghav <samir.aghav@shreeshanthaitsolutions.com>
2023-04-19 10:46:26 +0530

    projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Container } from "./styles";
 // import githubIcon from "../../assets/github.svg";
 // import DownloadApp from "../../assets/download.png";
@@ -10,6 +10,94 @@ export function Project() {
     <Container id="project">
       <h2>My Projects</h2>
       <div className="projects">
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://giphyapplication.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Giphy Application</h3>
+              <p>
+                Developed a Giphy search application using React and the Giphy
+                API, allowing users to search for and browse through a vast
+                library of GIFs.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Html</li>
+                <li>css</li>
+                <li>Styled Components</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://giphyapplication.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Portfolio</h3>
+              <p>
+                Created my personal portfolio using React Js and TypeScript.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Html</li>
+                <li>css</li>
+                <li>TypeScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
