3704f0bc5708b842debbe6bfa625bf5f4369a536
samir aghav <samir.aghav@shreeshanthaitsolutions.com>
2023-05-02 08:54:49 +0530

    projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -163,6 +163,52 @@ export function Project() {
               </svg>
               <div className="project-links">
                 <a
+                  href="https://anime-website-samiraghav.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Anime Website</h3>
+              <p>
+                Developed a responsive anime website using ReactJS and CSS,
+                featuring a modern and intuitive user interface.Utilized the
+                Jikan API to fetch anime data, including titles, descriptions,
+                episode lists, and ratings, to populate the website's content.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Html</li>
+                <li>css</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
                   href="https://color-paletteapplication.netlify.app/"
                   target="_blank"
                   rel="noreferrer"
