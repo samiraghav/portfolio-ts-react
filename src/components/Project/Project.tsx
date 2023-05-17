beb3194f606588ffcb85603982ea4429bab23537
Shivam Rushiji <samiraghav24@gmail.com>
2023-05-17 10:24:51 +0530

    new project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -10,6 +10,53 @@ export function Project() {
     <Container id="project">
       <h2>My Projects</h2>
       <div className="projects">
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://code-ding.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Code Ding</h3>
+              <p>
+                Created a playground area where users can create multiple code snippets and run them using an online compiler and
+                execution system called Judge0.Developed a Code Editor frontend using ReactJS and utilized the CodeMirror package for the editor component,
+                providing a user-friendly coding interface.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>API</li>
+                <li>Html</li>
+                <li>css</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
